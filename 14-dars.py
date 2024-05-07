f97abe909c2f1edc491b59f21f8c813fd98b2b26
NasibaAbdullayevna <168542699+NasibaAbdullayevna@users.noreply.github.com>
2024-05-07 06:52:32 +0500

    14-dars javoblari
    Dictionary mavzusidagi amaliyni bajardim

---
+++ 14-dars.py
@@ -0,0 +1,23 @@
+"""
+python 14-dars "Lug'at bilan tanishuv"
+Nasiba Abdulloyevna
+07.05.2024
+"""
+"""otam={'ism':'abdullo','t_yili':1958,'manzil':'losha'}
+print(f"Otamning ismlari {otam['ism'].title()}, {otam['t_yili']}-yilda, \
+{otam['manzil'].title()}da tug'ilgan")"""
+
+"""taomlar={'anvar':'manti','kamola':'shashlik','akbarali':'kabob','sarvar':'sut','nasiba':'osh'}
+print(f"Anvarning sevimli taomi {taomlar['anvar']}")
+print(f"Kamolaning sevimli taomi {taomlar['kamola']}")
+print(f"Sarvarning sevimli taomi {taomlar['sarvar']}")"""
+
+"""izohlar={'integer':'butun tipdagi o\'zgaruvchi','float':"kasr ko'rinishidagi o'zgaruvchi",\
+         'string':"matn tipidagi o'zgaruvchi",'if':'shart operatori','for':'takrorlanish operatori',\
+         'dictionary':"lug'at",'list':"ro'yxat",'tupl':"o'zgarmas ro'yxat"}
+kalit=input('Kalit so\'zni kiriting: ')
+emas=izohlar.get(kalit)
+if emas==None:
+    print("Bunday so'z mavjud emas")
+else:
+    print(f"{kalit.title()} so'zi {izohlar[kalit]} deb tarjima qilinadi")"""
